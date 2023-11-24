52383b47bd3925b718e2fb83e467bae683e0263c
TouristOnAMission <maikl.mikhael@yandex.ru>
2023-11-24 20:52:37 +0300

    Лекция 4

---
+++ yandex_contest/training1/lecture4/lecture4.js
@@ -0,0 +1,66 @@
+// Задача 1
+
+// Дано два числа X и Y без ведущих нулей.
+// Необходимо проверить можно ли получить первое из второго перестановкой цифр
+
+const checkDigitPermutations = (x, y) => {
+  const countDigits = (num) => {
+    const digitArr = new Array(10).fill(0);
+
+    while (num > 0) {
+      digitArr[num % 10]++;
+      num = Math.floor(num / 10);
+    }
+
+    return digitArr;
+  };
+
+  const countX = countDigits(x);
+  const countY = countDigits(y);
+
+  for (let i in countX) {
+    if (countX[i] !==countY[i]) return false;
+  }
+
+  return true;
+};
+
+console.log(checkDigitPermutations(123, 321));
+
+// Задача 2
+
+// На шахматной доске N * N находятся M ладей (ладья бьет клетки на той же горизонтали или вертикали до ближайшей занятой)
+// Определить сколько пар ладей бьют друг друга. Ладьи задаются парой чисел I, J - координаты клетки
+// 1 <= N <= 10 ** 9, 0 <= M <= 2 * 10 ** 5
+
+const countRookPairs = (rookcoords) => {
+  const row = {};
+  const col = {};
+
+  const fillDict = (dictionary, key) => {
+    if (dictionary[key]) {
+      dictionary[key]++;
+    } else {
+      dictionary[key] = 1;
+    }
+  };
+
+  const countPairs = (dictionary) => {
+    let pairs = 0;
+
+    for (let key in dictionary) {
+      pairs += dictionary[key] - 1;
+    }
+
+    return pairs;
+  };
+
+  for (let coord of rookcoords) {
+    fillDict(row, coord[0]);
+    fillDict(col, coord[1]);
+  }
+
+  return countPairs(row) + countPairs(col);
+};
+
+console.log(countRookPairs([[1, 1], [1, 2], [3, 4], [5, 4], [3, 3]]));
