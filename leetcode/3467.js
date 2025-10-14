2eb8883ce30a15ba5497483c587c38fcd6a5f0a4
sdlmdev <sdlmdev@yandex.ru>
2025-10-14 23:39:27 +0300

    update

---
+++ leetcode/3467.js
@@ -0,0 +1,45 @@
+// 3467. Transform Array by Parity
+//
+// You are given an integer array nums. Transform nums by performing the following operations in the exact order specified:
+// Replace each even number with 0.
+// Replace each odd numbers with 1.
+// Sort the modified array in non-decreasing order.
+// Return the resulting array after performing these operations.
+//
+// Example 1:
+// Input: nums = [4,3,2,1]
+// Output: [0,0,1,1]
+// Explanation:
+// Replace the even numbers (4 and 2) with 0 and the odd numbers (3 and 1) with 1. Now, nums = [0, 1, 0, 1].
+// After sorting nums in non-descending order, nums = [0, 0, 1, 1].
+//
+// Example 2:
+// Input: nums = [1,5,1,4,2]
+// Output: [0,0,1,1,1]
+// Explanation:
+// Replace the even numbers (4 and 2) with 0 and the odd numbers (1, 5 and 1) with 1. Now, nums = [1, 1, 1, 0, 0].
+// After sorting nums in non-descending order, nums = [0, 0, 1, 1, 1].
+//
+// Constraints:
+//
+// 1 <= nums.length <= 100
+// 1 <= nums[i] <= 1000
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var transformArray = function(nums) {
+  const res = new Array(nums.length).fill(0);
+
+  for (let i = 0, lastOddIndex = nums.length - 1; i < nums.length; i += 1) {
+    const curNum = nums[i];
+
+    if (curNum % 2 !== 0) {
+      res[lastOddIndex] = 1;
+      lastOddIndex -= 1;
+    }
+  }
+
+  return res;
+};
